32318032b79590efd7cfa396f0f67d56df54adca
Leticia Araoz <araoz.leticia@gmail.com>
2023-07-25 23:33:41 -0300

    feat: create FavFilter.js

---
+++ src/components/FavFilter.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { BsFillStarFill, BsStar } from "react-icons/bs";
+import themes from "../themes";
+
+export default function () {
+  return <BsStar color={themes.colors.borders} size={20} />;
+}
