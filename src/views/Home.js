fdb22e89dd97629d5a3d056603040fb23deb4aed
Leticia Araoz <araoz.leticia@gmail.com>
2023-07-27 09:05:26 -0300

    feat: home random character

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,3 +1,25 @@
+import { useContext, useMemo } from "react";
+import Card from "../components/Card";
+import { CardsContainer } from "../components/layout/CardsContainer";
+import { GlobalStateContext } from "../store";
+import useCharacterSearch from "../hooks/useCharacterSearch";
+import { PopularCharacters as chars } from "../constants";
+
 export default function Home() {
-  return <h1>HOME</h1>;
+  const { searchResult } = useContext(GlobalStateContext);
+  const randomChar = useMemo(() => {
+    return chars[Math.floor(Math.random() * chars.length)];
+  }, []);
+
+  useCharacterSearch({
+    query: randomChar,
+  });
+
+  return (
+    <CardsContainer>
+      {searchResult.map((character) => {
+        return <Card key={character.id} character={character} />;
+      })}
+    </CardsContainer>
+  );
 }
